5e2c3da95b2c941b627a6259e11b93c7dc1ba97c
astrlux <37908451+astrlux@users.noreply.github.com>
2023-11-14 13:49:05 -0800

    augment paths

--- ford_fulkerson.ex
+++ ford_fulkerson.ex
@@ -31,6 +31,32 @@ defmodule FordFulkerson do
     max_flow
   end
 
+  # Private function to build the residual graph as a copy of the original graph
+  defp build_residual_graph(graph) do
+    Enum.map(graph, fn {u, v, capacity} -> {u, v, capacity, capacity} end)
+  end
 
+  # Private function to find an augmenting path in the residual graph using BFS
+  defp find_augmenting_path(graph, source, sink) do
+    queue = [{source, []}]
+
+    while !Enum.empty?(queue) do
+      {current, path} = Enum.pop(queue)
+
+      # Check if the current vertex is the sink
+      if current == sink do
+        # Return the augmenting path if found
+        return path
+      end
+
+      # Explore neighbors and add them to the queue
+      neighbors = Enum.filter(graph, fn {u, v, capacity, _} -> u == current and capacity > 0 end)
+      new_paths = Enum.map(neighbors, fn {_, v, _, _} -> {v, path ++ [{current, v}]} end)
+      queue = queue ++ new_paths
+    end
+
+    # Return nil if no augmenting path is found
+    nil
+  end
 
 end
